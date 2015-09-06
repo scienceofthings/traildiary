50839eeb85f23fae8874916fce9f886305943331
Gregor Hyneck <gregor@hyneck.de>
2015-09-06 19:37:03 +0200

    delete AppBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
             new Ghyneck\MapBundle\MapBundle(),
             new Bmatzner\JQueryUIBundle\BmatznerJQueryUIBundle(),
             new Bmatzner\JQueryBundle\BmatznerJQueryBundle(),
