6279484fa9257b948b46be33ccc72eb4270f46b9
Gregor Hyneck <gregor@hyneck.de>
2015-10-06 19:42:44 +0200

    add entity category

---
+++ src/Ghyneck/MapBundle/Entity/Category.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Ghyneck\MapBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Category
+ */
+class Category
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Category
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+}
