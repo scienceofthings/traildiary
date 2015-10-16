a8ab0015218cbe377dda9dd1bc3e1c633d40f69d
Gregor Hyneck <gregor@hyneck.de>
2015-10-16 14:07:18 +0200

    add class for building the knp Menu

---
+++ src/Ghyneck/MapBundle/Menu/Builder.php
@@ -0,0 +1,27 @@
+<?php
+namespace Ghyneck\MapBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+class Builder extends ContainerAware
+{
+    /*
+     * @param FactoryInterface $factory
+     * array $options
+     *
+     * @return Knp\Menu
+     */
+    public function mainMenu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root', array(
+            'navbar' => true,
+        ));
+
+        $layout = $menu->addChild('Layout', array(
+            'route' => 'tour',
+        ));
+
+        return $menu;
+    }
+}
