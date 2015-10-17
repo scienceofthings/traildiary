d9d2c4a92d6e90dfb09d2029cd82a25cadd19753
Gregor Hyneck <gregor@hyneck.de>
2015-10-17 19:06:22 +0200

    find tour given by parameter by directory in order to add files

--- src/Ghyneck/MapBundle/Command/ImportCommand.php
+++ src/Ghyneck/MapBundle/Command/ImportCommand.php
@@ -31,17 +31,40 @@ class ImportCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $directory = $input->getArgument('directory');
-        if ($directory) {
-            $text = 'Hello '. $directory;
-        } else {
-            $text = 'Hello';
-        }
+        $uploadDestination = $this->getUploadDestination();
 
         if ($input->getOption('all')) {
-            $text = strtoupper($text);
+            $this->importAllDirectories($uploadDestination);
+            $output->writeln("All directories imported.");
+            return;
         }
 
-        $output->writeln($text);
+        $directory = $input->getArgument('directory');
+        if ($directory) {
+            $this->importDirectory($uploadDestination, $directory);
+            $output->writeln("Directory $directory imported.");
+        } else {
+            $output->writeln("No directories imported.");
+        }
+    }
+
+    protected function getUploadDestination()
+    {
+        $vichUploaderMappings = $this->getContainer()->getParameter('vich_uploader.mappings');
+        $uploadDestination = $vichUploaderMappings['image']['upload_destination'];
+        return $uploadDestination;
+    }
+
+    protected function importAllDirectories($uploadDestination)
+    {
+
+    }
+
+    protected function importDirectory($uploadDestination, $directory)
+    {
+        $em = $this->getContainer()->get('doctrine')->getManager();
+        $tour = $em->getRepository('MapBundle:Tour')->findOneByDirectory($directory);
+        // tour-addImages
+        // tour->setGpxFile
     }
 }
