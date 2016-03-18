be36be963e4e7f8615a17dfe9fcb4b5eb0853329
Gregor Hyneck <supergreg@gmx.de>
2016-03-18 19:48:05 +0100

    add newAction

--- src/Ghyneck/MapBundle/Controller/CategoryController.php
+++ src/Ghyneck/MapBundle/Controller/CategoryController.php
@@ -3,6 +3,8 @@
 namespace Ghyneck\MapBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Ghyneck\MapBundle\Entity\Category;
+use Ghyneck\MapBundle\Form\CategoryType;
 
 class CategoryController extends Controller
 {
@@ -16,16 +18,52 @@ class CategoryController extends Controller
             'entities' => $entities,
         ));    }
 
-    public function createAction()
+    public function createAction(Request $request)
     {
+        $category = new Category();
+        $form = $this->createCreateForm($category);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($category);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('category_show', array('id' => $category->getId())));
+        }
+
         return $this->render('MapBundle:Category:create.html.twig', array(
-                // ...
-            ));    }
+            'entity' => $category,
+            'form'   => $form->createView(),
+            ));
+    }
+
+    /**
+     * Creates a form to create a Tour entity.
+     *
+     * @param Tour $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Category $entity)
+    {
+        $form = $this->createForm(new CategoryType(), $entity, array(
+            'action' => $this->generateUrl('category_create'),
+            'method' => 'POST',
+        ));
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $form;
+    }
 
     public function newAction()
     {
+        $entity = new Category();
+        $form = $this->createCreateForm($entity);
+
         return $this->render('MapBundle:Category:new.html.twig', array(
-                // ...
+            'entity' => $entity,
+            'form'   => $form->createView(),
             ));    }
 
     public function showAction()
